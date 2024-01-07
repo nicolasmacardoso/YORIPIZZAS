58c43ee2b82ae3102cfa961faf4537f795defb0e
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-01-06 22:48:07 -0300

    Criptografando a senha

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,5 +1,5 @@
 import prismaClient from "../../prisma";
-
+import { hash } from 'bcryptjs'
 interface UserRequest{
     name: string;
     email: string;
@@ -25,11 +25,13 @@ class CreateUserService {
             throw new Error('Esse email já está cadastrado!')
         }
 
+        const passwordHash = await hash(password, 8)
+
         const user = await prismaClient.user.create({
             data: {
                 name: name,
                 email: email,
-                password: password
+                password: passwordHash,
             },
             select: {
                 id: true,
