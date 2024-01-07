ba1bd1100744e07188a92e63cf4d5882f26c7205
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-01-07 12:51:13 -0300

    Rota produtos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,8 @@ import { DetailUserController } from './controllers/user/DetailUserController';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
 
+import { CreateProductController } from './controllers/product/CreateProductController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
@@ -21,6 +23,9 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 //-- ROTAS CATEGORY --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
-router.get('/category', isAuthenticated,new ListCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+//-- ROTAS PRODUCT --
+router.post('/product', isAuthenticated, new CreateProductController().handle)
 
 export { router };
