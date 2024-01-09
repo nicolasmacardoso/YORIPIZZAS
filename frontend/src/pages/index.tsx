9b65b13515a2ce600916b82f6fd149e359f44145
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-01-09 17:18:11 -0300

    Criando tela login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,10 @@
 import Head from "next/head"
+import Image from "next/image"
+import styles from '../../styles/home.module.scss'
+
+import logoImg from '../../public/logo.svg'
+
+import { Input } from "@/components/ui/Input"
 
 export default function Home() {
   return (
@@ -6,8 +12,23 @@ export default function Home() {
       <Head>
         <title>Yoripizza - Faça seu login</title>
       </Head>
-      <div>
-        <h1>Yoripizzas</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Yoripizzas"/>
+
+        <div className={styles.login}>
+          <form>
+            <Input
+              placeholder="Digite seu email"
+              type="text"
+            />
+
+            <Input
+              placeholder="Digite sua senha"
+              type="password"
+            />
+
+          </form>
+        </div>
       </div>
     </>
   )
