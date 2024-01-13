d63ee16f1340cd79c7c3897a2b0116e7761fde78
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-01-13 20:04:34 -0300

    Configurando modal

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -8,6 +8,10 @@ import { FiRefreshCcw } from "react-icons/fi"
 
 import { setupAPIClient } from "@/services/api"
 
+import { ModalOrder } from "@/components/ModalOrder"
+
+import Modal from 'react-modal';
+
 type OrderProps = {
   id: string;
   table: string | number;
@@ -19,13 +23,51 @@ interface HomeProps {
   orders: OrderProps[];
 }
 
+export type OrderItemProps = {
+  id: string;
+  amount: number;
+  order_id: string;
+  product_id: string;
+  product: {
+    id: string;
+    name: string;
+    description: string;
+    price: string;
+    banner: string;
+  }
+  order: {
+    id: string;
+    table: string | number;
+    status: boolean;
+    name: string | null;
+  }
+}
+
 export default function Dashboard({ orders }: HomeProps){
 
   const [orderList, setOrderList] = useState(orders || [])
 
-  function handleOpenModalView(id: string) {
-    alert("ID Clicado: " + id)    
+  const [modalItem, setModalItem] = useState<OrderItemProps[]>()
+  const [modalVisible, setModalVisible] = useState(false)
+
+  function handleCloseModal(){
+    setModalVisible(false);
   }
+
+  async function handleOpenModalView(id: string) {
+    const apiCliente = setupAPIClient();
+
+    const response = await apiCliente.get('order/detail', {
+      params: {
+        order_id: id,
+      }
+    })
+
+    setModalItem(response.data);
+    setModalVisible(true);
+  }
+
+  Modal.setAppElement('#__next');
 
   return(
     <>
@@ -56,6 +98,11 @@ export default function Dashboard({ orders }: HomeProps){
 
         </article>
       </main>
+
+      { modalVisible && (
+        <ModalOrder/>
+      )}
+
     </div>
     </>
   )
