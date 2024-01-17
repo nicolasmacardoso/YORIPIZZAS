59f38156a408189ef2f8f1b43f3ce2c8c9201f3a
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-01-16 21:31:43 -0300

    Tela login pt 1

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function SignIn() {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Tela de login</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({ 
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1D1D2E',
+    }
+})
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View } from "react-native";
+import { View, ActivityIndicator} from "react-native";
 
 import AuthRoutes from "./auth.routes";
 import AppRoutes from "./app.routes";
@@ -8,6 +8,21 @@ import AppRoutes from "./app.routes";
 function Routes() {
     const isAuthenticated = false;
     const loading = false;
+
+    if(loading) {
+        return (
+            <View 
+                style={{ 
+                    flex: 1, 
+                    backgroundColor: '#1D1D2E', 
+                    justifyContent: 'center', 
+                    alignItems: 'center'
+                }}
+            >
+                <ActivityIndicator size={60} color="#FFF"/>
+            </View>
+        )
+    }
 
     return (
         isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
