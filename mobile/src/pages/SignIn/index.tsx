aeac9ff62a67b34b14276e0fe700a50c7a7eda23
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-01-18 20:17:06 -0300

    Funcionalidade login

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -11,15 +11,17 @@ import {
 import { AuthContext } from "../../contexts/AuthContext";
 
 export default function SignIn() {
-    const { user } = useContext(AuthContext)
+    const { signIn } = useContext(AuthContext)
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    function handleLogin() {
+    async function handleLogin() {
         if (email === ''|| password === '') {
             return;
         }
+        
+        await signIn({ email, password })
     }
 
     return (
