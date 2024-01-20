b03c240effb5cbede93bfbdccf2de36fccdb1c63
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-01-19 21:28:18 -0300

    Abrindo a mesa

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,11 +1,26 @@
 import React, { useContext, useState } from "react";
 import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from "react-native";
 
-import { AuthContext } from "../../contexts/AuthContext";
+import { useNavigation } from "@react-navigation/native";
+
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackParamsList } from "../../routes/app.routes";
 
 export default function Dashboard(){
-    const { signOut } = useContext(AuthContext);
+    const navigation = useNavigation<NativeStackNavigationProp<StackParamsList>>();
+
+    const [number, setNumber] = useState('');
+
     
+    async function openOrder() {
+        if (number === '') {
+            return;
+        }
+
+        navigation.navigate('Order', { number: number, order_id: '' })
+
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Novo pedido</Text>
@@ -15,9 +30,11 @@ export default function Dashboard(){
                 placeholderTextColor="white"
                 style={styles.input}
                 keyboardType="numeric"
+                value={number}
+                onChangeText={setNumber}
             />
 
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={openOrder}>
                 <Text style={styles.buttonText}>Abrir mesa</Text>
             </TouchableOpacity>
         
