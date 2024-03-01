68a71920731c3dc0b388413960eac91bd494c62e
Nícolas Machado Cardoso <106183309+nicolasmacardoso@users.noreply.github.com>
2024-03-01 20:49:07 -0300

    Mexendo na função de deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,4 +30,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     })
 })
 
-app.listen(3333, () => console.log('Servidor rodando!'));
+app.listen(process.env.PORT, () => console.log('Servidor rodando!'));
